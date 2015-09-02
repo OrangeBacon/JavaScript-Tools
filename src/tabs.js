5ae3c669d8e3a0b37fc589ab115f5e1b8ba161e2
ScratchOs <ScratchOs@users.noreply.github.com>
2015-09-02 17:14:18 +0100

    Update tabs.js

--- src/tabs.js
+++ src/tabs.js
@@ -1,1 +1,6 @@
-
+function tab(tabId, pageId) {
+  $("div#tabs" + tabId + "> div.page").css('display', 'none');
+  $("div#tabs" + tabId + "> div#page" + pageId).css('display', 'inline');
+  $("ul#tabs" + tabId + "> li").css('background-color', '#A0A0A0');
+  $("ul#tabs" + tabId + "> li#page" + pageId).css('background-color', 'white');
+};
