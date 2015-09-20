9c3a0fd9bbd4316e643c9a47569dea557635ecad
ScratchOs <ScratchOs@users.noreply.github.com>
2015-09-20 10:50:43 +0100

    Update tabs.js

--- src/tabs.js
+++ src/tabs.js
@@ -6,7 +6,7 @@ function tab(tabId, pageId) {
   $("ul#tabs" + tabId + "> li#page" + pageId).css('background-color', 'white');
 };
 
-The MIT License (MIT)
+/*The MIT License (MIT)
 
 Copyright (c) 2015 ScratchOs
 
@@ -27,3 +27,4 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
