89a4c7055dd9f843b9b48f0b08d420ef190f2d6d
ScratchOs <ScratchOs@users.noreply.github.com>
2015-09-27 14:05:42 +0100

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -1,11 +1,13 @@
 function tab(tabId, pageId) {
-  event.preventDefault()
+  event.preventDefault();
   $("div#tabs" + tabId + "> div.page").css('display', 'none');
   $("div#tabs" + tabId + "> div#page" + pageId).css('display', 'inline');
   $("ul#tabs" + tabId + "> li").css('background-color', '#A0A0A0');
   $("ul#tabs" + tabId + "> li#page" + pageId).css('background-color', 'white');
 };
 
-function spoil() {
-  
+function spoil(id) {
+  event.preventDefault();
+  $("#spoiler" + id).slideToggle( "slow" );
+  $(this).text('PUSH ME' ? 'DON"T PUSH ME' : 'PUSH ME');
 };
