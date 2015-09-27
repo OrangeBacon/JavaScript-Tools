34efdf62f3f4b9a6da2a1266dd56f03591834a77
ScratchOs <ScratchOs@users.noreply.github.com>
2015-09-27 13:48:28 +0100

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -4,5 +4,8 @@ function tab(tabId, pageId) {
   $("div#tabs" + tabId + "> div#page" + pageId).css('display', 'inline');
   $("ul#tabs" + tabId + "> li").css('background-color', '#A0A0A0');
   $("ul#tabs" + tabId + "> li#page" + pageId).css('background-color', 'white');
-  $("ul.tabs>li:hover").css('background-color', '#D0D0D0');
+};
+
+function spoil() {
+  
 };
