03fa3f68b0365feabe1fbe38f00c0aa6f5a0f788
-ScratchOs <ScratchOs@users.noreply.github.com>
2015-10-12 20:16:03 +0100

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -27,7 +27,7 @@ function spoil(id) {
   }
 };
 
-document.onload = function(){
+window.onload = function(){
   var i1 = document.querySelectorAll('.show');
   for (var i = 0; i < i1.length; i++) {
     i1[i].setAttribute("onclick", "this\.innerHTML \=\=\= \'Show\' \? this\.innerHTML \= \'Hide\'\: this\.innerHTML \= \'Show\'\;" + i1[i].getAttribute("onclick"));
