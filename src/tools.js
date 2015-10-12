d3aca340fefb2c85a7d795dca90c2069174c1c8f
-ScratchOs <ScratchOs@users.noreply.github.com>
2015-10-12 20:24:26 +0100

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -31,5 +31,6 @@ window.onload = function(){
   var i1 = document.querySelectorAll('.show');
   for (var i = 0; i < i1.length; i++) {
     i1[i].setAttribute("onclick", i1[i].getAttribute("onclick") + "\; this\.innerHTML \=\=\= \'Show\' \? this\.innerHTML \= \'Hide\'\: this\.innerHTML \= \'Show\'\;");
+    i1[i].style.display = 'none';
   };
 };
