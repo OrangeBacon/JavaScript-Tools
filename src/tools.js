db5cb15a25187fc238285c3c6a71355d0284cbbc
-ScratchOs <ScratchOs@users.noreply.github.com>
2015-10-12 20:21:40 +0100

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -30,6 +30,6 @@ function spoil(id) {
 window.onload = function(){
   var i1 = document.querySelectorAll('.show');
   for (var i = 0; i < i1.length; i++) {
-    i1[i].setAttribute("onclick", i1[i].getAttribute("onclick") + "this\.innerHTML \=\=\= \'Show\' \? this\.innerHTML \= \'Hide\'\: this\.innerHTML \= \'Show\'\;");
+    i1[i].setAttribute("onclick", i1[i].getAttribute("onclick") + "\; this\.innerHTML \=\=\= \'Show\' \? this\.innerHTML \= \'Hide\'\: this\.innerHTML \= \'Show\'\;");
   };
 };
