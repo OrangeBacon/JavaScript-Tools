ad7465a2750c069121d250829a171bdd4cbeb824
-ScratchOs <ScratchOs@users.noreply.github.com>
2015-10-12 17:47:00 +0100

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -1,18 +1,18 @@
 function tab(tabId, pageId) {
   event.preventDefault();
-  var i1 = [document.querySelector("div#tabs" + tabId + "> div.page");]
+  var i1 = [document.querySelector("div#tabs" + tabId + "> div.page")];
   for (var i = 0; i < i1.length; i++) {
     i1[i].style.display = 'none';
   };
-  i1 = [document.querySelectorAll("div#tabs" + tabId + "> div#page" + pageId);]
+  i1 = [document.querySelectorAll("div#tabs" + tabId + "> div#page" + pageId)];
   for (i = 0; i < i1.length; i++) {
     i1[i].style.display = 'inline';
   };
-  i1 = [document.querySelectorAll("ul#tabs" + tabId + "> li");]
+  i1 = [document.querySelectorAll("ul#tabs" + tabId + "> li")];
   for (i = 0; i < i1.length; i++) {
     i1[i].style.backgroundColor = '#A0A0A0';
   };
-  i1 = [document.querySelectorAll("ul#tabs" + tabId + "> li#page" + pageId);]
+  i1 = [document.querySelectorAll("ul#tabs" + tabId + "> li#page" + pageId)];
   for (i = 0; i < i1.length; i++) {
     i1[i].style.backgroundColor = 'white';
   };
