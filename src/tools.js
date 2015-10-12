26f506048deb2ad39df420fbdcfa42d8bfbfb14f
-ScratchOs <ScratchOs@users.noreply.github.com>
2015-10-12 17:48:34 +0100

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -4,15 +4,15 @@ function tab(tabId, pageId) {
   for (var i = 0; i < i1.length; i++) {
     i1[i].style.display = 'none';
   };
-  i1 = [document.querySelectorAll("div#tabs" + tabId + "> div#page" + pageId)];
+  i1 = document.querySelectorAll("div#tabs" + tabId + "> div#page" + pageId);
   for (i = 0; i < i1.length; i++) {
     i1[i].style.display = 'inline';
   };
-  i1 = [document.querySelectorAll("ul#tabs" + tabId + "> li")];
+  i1 = document.querySelectorAll("ul#tabs" + tabId + "> li");
   for (i = 0; i < i1.length; i++) {
     i1[i].style.backgroundColor = '#A0A0A0';
   };
-  i1 = [document.querySelectorAll("ul#tabs" + tabId + "> li#page" + pageId)];
+  i1 = document.querySelectorAll("ul#tabs" + tabId + "> li#page" + pageId);
   for (i = 0; i < i1.length; i++) {
     i1[i].style.backgroundColor = 'white';
   };
